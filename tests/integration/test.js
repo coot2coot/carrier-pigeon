d1d164c52a361708484651eaaf1f8f608943e2f5
NataliaLKB <natalialeebaltazar@gmail.com>
2015-03-22 22:09:13 +0000

    adding a couple more basic tests

---
+++ tests/integration/test.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+var shot = require('shot');
+var handler = require('../../server/handler.js');
+
+
+test('home handler redirects to login page', function (t) {
+
+	shot.inject(handler.home, { method: 'get', url: '/'}, function (res) {
+		t.equal(res.statusCode, 302, "response code is correct");
+		t.equal(res.headers.Location, "/login", "redirect location is correct");
+		t.end();
+	});	
+});
+
+test('once on the login page', function (t) {
+
+	shot.inject(handler.login, { method: 'get', url: '/login'}, function (res) {
+		console.log(payload);
+		t.equal(res.statusCode, 200, "response code is correct");
+
+		var payload = res.payload.indexOf('Login') > -1;
+
+		t.equal(payload, true, "redirect location is correct");
+		t.end();
+	});	
+});
