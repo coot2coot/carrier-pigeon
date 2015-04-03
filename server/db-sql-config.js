89ab3df8462cb3795143ab4234afd1c2473a4669
benjaminlees <benji.man.lees@gmail.com>
2015-04-03 18:32:47 +0100

    moving to progress

---
+++ server/db-sql-config.js
@@ -0,0 +1,60 @@
+var pg = require("pg");
+var param = "postgres://benjaminlees:pofywbi0@aa2xurcp6gqkuq.cbp7s6d11jp7.eu-west-1.rds.amazonaws.com:5432/carrier-pigeon-dev";
+;
+var client = new pg.Client(param);
+var dataBase = {};
+
+
+var connect = function (query,table,cb,cos) {
+	client.connect(function(err) {
+	  	if(err) {
+	    	return console.error('could not connect to postgres', err);
+	  	}
+	  	query(table,cb,doc);
+	});
+}
+
+function get(table, cb) {
+	client.query('SELECT * FROM '+ table +';', function(err, result) {
+	    if(err) {
+	      return console.error('error running query', err);
+	    }
+	    cb(result.rows);
+	    client.end();
+	});
+}
+
+function put(table, cb, doc) {
+	client.query('INSERT INTO ' + table + ' ' + doc.columns' VALUES ' + doc.values, function(err, result) {
+	    if(err) {
+	      return console.error('error running query', err);
+	    }
+	    client.end();
+	    cb();
+	});
+}
+
+function remove(table, cb, doc) {
+	client.query('DELETE FROM ' + table + ' WHERE ' + cd.column' = ' + cd.column.values, function(err, result) {
+	    if(err) {
+	      return console.error('error running query', err);
+	    }
+	    client.end();
+	    cb();
+	});
+}
+
+dataBase.get = function (table, cb){
+ 	connect(get,table,cb);
+};
+
+dataBase.put = function (table, cb, doc){
+ 	connect(put,table,cb,doc);
+};
+
+dataBase.remove = function (table, cb, doc){
+ 	connect(remove,table,cb,doc);
+};
+
+
+module.exports = dataBase;
