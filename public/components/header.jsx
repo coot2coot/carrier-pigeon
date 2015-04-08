7e367614c5e9d61a8e57b876f443e0e9c3a95bac
NataliaLKB <natalialeebaltazar@gmail.com>
2015-04-08 13:50:58 +0100

    adding basic styling to nav to make it more like xero

--- public/components/header.jsx
+++ public/components/header.jsx
@@ -2,21 +2,40 @@
 var React = require('react');
 var Navbar = require("./nav.jsx");
 
-var Buttons = React.createClass({
-   	render: function() {
-      	return (
-			<div>
-				<a href="/login">
-					<p>Logout</p>
-				</a>
-				<p>Welcome username</p>
-			</div>
-		)
-	}
-});
-
 //TODO: Make this dependent on real authentication
 var isAuthenticated = true;
+var isAdmin = true;
+
+
+var AdminLink = React.createClass({
+    render: function() {
+        return (
+            <a href="/login">
+                <p>Admin Panel</p>
+            </a>
+        )
+    }
+});
+
+
+// {(isAdmin
+//     ? <AdminLink />     
+//     : <div>Never showing false item</div>
+// )}
+
+
+var Buttons = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <a href="/login">
+                    <p>Logout</p>
+                </a>
+                <p>Welcome username</p>
+            </div>
+        )
+    }
+});
 
 module.exports = React.createClass({
     login: function(e) {
@@ -25,20 +44,24 @@ module.exports = React.createClass({
     render: function() {
         return (
             <header className="container-fluid">
-	            <div className="row">
-	                <div className="column-6 push-2">
-	                    <h1>Coot Freight Ltd</h1>
-	                </div>
-	                {(isAuthenticated
-				        ? <Buttons />     
-				        : <div>Never showing false item</div>
-				    )}
-	            </div>
-	            {(isAuthenticated
-			        ? <Navbar />     
-			        : <div>Never showing false item</div>
-			    )}
-	        </header>
+                <div className="row">
+                    <div className="column-6 push-1">
+                        <h1>Coot Freight Ltd</h1>
+                    </div>
+                    <div>
+                    {(isAuthenticated
+                        ? <Buttons />     
+                        : <div>Never showing false item</div>
+                    )}
+                    </div>
+                    <div className="column-12 push-1">
+                        {(isAuthenticated
+                            ? <Navbar />     
+                            : <div>Never showing false item</div>
+                        )}
+                    </div>
+                </div>
+            </header>
         );
     }
 })
--- public/components/nav.jsx
+++ public/components/nav.jsx
@@ -7,27 +7,51 @@ module.exports = React.createClass({
     },
     render: function() {
         return (
-            <div className="row nav">
-				<div className="column-4 push-2">
-					<a href="">
-						<img src="./img/nav/shipping.png" />
-						<br />
-						<p>Orders</p>
-					</a>
-				</div>
-				<div className="column-4 push-2">
-					<a href="">
-						<img src="./img/nav/booking.png" />
-						<p>Booking notice</p>
-					</a>
-				</div>
-				<div className="column-4 push-2">
-					<a href="">
-						<img src="./img/nav/list.png" />
-						<p>Client</p>
-					</a>
-				</div>
-			</div>
+
+        	<nav className="nav-responsive">
+			  	<ul className="list-inline">
+			    	<li>
+				    	<a href="">
+							<img src="../img/nav/shipping.png" />
+							<h5>Orders</h5>
+						</a>
+					</li>
+					<li>
+				    	<a href="">
+							<img src="../img/nav/booking.png" />
+							<h5>Booking notice</h5>
+						</a>
+					</li>
+					<li>
+				    	<a href="">
+							<img src="../img/nav/list.png" />
+							<h5>Client</h5>
+						</a>
+					</li>
+			  	</ul>
+			</nav>
         );
     }
 })
+
+// <div className="row nav">
+// 				<div className="column-4 push-2">
+// 					<a href="">
+// 						<img src="./img/nav/shipping.png" />
+// 						<br />
+// 						<p>Orders</p>
+// 					</a>
+// 				</div>
+// 				<div className="column-4 push-2">
+// 					<a href="">
+// 						<img src="./img/nav/booking.png" />
+// 						<p>Booking notice</p>
+// 					</a>
+// 				</div>
+// 				<div className="column-4 push-2">
+// 					<a href="">
+// 						<img src="./img/nav/list.png" />
+// 						<p>Client</p>
+// 					</a>
+// 				</div>
+// 			</div>
