aecd98936f0147f6df9de036337df660e0ee8b46
NataliaLKB <natalialeebaltazar@gmail.com>
2015-04-10 15:37:40 +0100

    adding header on each page

--- public/components/nav.jsx
+++ public/components/nav.jsx
@@ -1,35 +1,36 @@
 /** @jsx React.DOM */
-var React = require('react');
 
-module.exports = React.createClass({
-    login: function(e) {
-        alert("hello there");
-    },
-    render: function() {
-        return (
+module.exports = function(React, Link) {
+	return React.createClass({
+	    login: function(e) {
+	        alert("hello there");
+	    },
+	    render: function() {
+	        return (
 
-        	<nav className="nav">
-			  	<ul>
-			    	<li>
-				    	<a href="/orders">
-							<img src="../img/nav/shipping.png" />
-							<h5>Orders</h5>
-						</a>
-					</li>
-					<li>
-				    	<a href="/contacts">
-							<img src="../img/nav/list.png" />
-							<h5>Contacts</h5>
-						</a>
-					</li>
-					<li>
-				    	<a href="/reports">
-							<img src="../img/nav/booking.png" />
-							<h5>Reports</h5>
-						</a>
-					</li>
-			  	</ul>
-			</nav>
-        );
-    }
-})
+	        	<nav className="nav">
+				  	<ul>
+				    	<li>
+				    		<Link to = "orders">
+                                <img src="../img/nav/shipping.png" />
+								<h5>Orders</h5>
+                            </Link>
+						</li>
+						<li>
+							<Link to = "contacts">
+                                <img src="../img/nav/list.png" />
+								<h5>Contacts</h5>
+                            </Link>
+						</li>
+						<li>
+							<Link to = "reports">
+                                <img src="../img/nav/booking.png" />
+								<h5>Reports</h5>
+                            </Link>
+						</li>
+				  	</ul>
+				</nav>
+	        );
+	    }
+	})
+}
