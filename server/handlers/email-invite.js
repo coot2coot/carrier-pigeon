d70a6884e150b294252646eeeefb161312c784af
NataliaLKB <natalialeebaltazar@gmail.com>
2015-05-18 01:20:35 +0100

    changing api key and moving it into credentials

--- server/handlers/email-invite.js
+++ server/handlers/email-invite.js
@@ -4,8 +4,8 @@ var parseData       = require('../lib/get-form-data.js');
 var validateUser    = require('../lib/validate-user.js');
 var db              = require("../db-config.js");
 
-var api_key = 'key-c675f88a6e54632efaed14a61a431cf9';
-var domain = 'sandbox7b603196f4c643979486b6cce726860a.mailgun.org';
+var api_key = process.env.MAIL_GUN_API_KEY || requre("../../credentials.json").mainGunApiKey;
+var domain = process.env.MAIL_GUN_DOMAIN || requre("../../credentials.json").mainGunDomain;
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 String.prototype.capitalize = function () {
