0208ec2d07bf77b38cc23e7731544327ecec92ac
benjaminlees <benji.man.lees@gmail.com>
2015-05-20 17:56:26 +0100

    adding tests

--- tests/integration/database-test.js
+++ tests/integration/database-test.js
@@ -6,21 +6,20 @@ var testDb = require('../createDb.js');
 var client   = "postgres://qzdwpgfrviqmcu:1hJBjZXlz_8pjTb9qjPUTHiQao@ec2-107-20-159-103.compute-1.amazonaws.com:5432/d6dar9ohioh4dh?ssl=true";
 
 
-var tests = function (table) {
+var tests = function () {
 	// DB.GET
 
 	test("get function returns the specified tables contents", function(t) {
 
 		var results;
 		var callback =  function (result){
-			t.equals(result[0].job_number, '$12567', "get method returned the rows from the orders table");
+			t.equals(result[0].job_number, 3248, "get method returned the rows from the orders table");
 		};
 
 		try {
-			db.get(table,callback, client);
+			db.get('orders',callback, client);
 		} catch(e) {
-			t.notOk(false, "post request to orders table did not work");
-			testDb.clearTable(table);
+			t.notOk(true, "post request to orders table did not work");
 		}
 
 	    t.end();
@@ -28,12 +27,12 @@ var tests = function (table) {
 
 	// DB.POST
 
-	test("post function works", function(t) {
-		var callback =  function (){
-			t.ok(true, "post request to orders table worked")
+	test("post function", function(t) {
+		var callback =  function (result){
+			t.equlas(result,null, "post request to units and orders table worked")
 		};
 		try {
-			db.post(table, testDb.mockObject, callback, client);
+			db.post('orders', testDb.mockOrdersUnits, callback, client);
 		} catch(e) {
 			t.notOk(false, "post request to orders table did not work");
 		}
@@ -41,41 +40,41 @@ var tests = function (table) {
 	    t.end();
 	});
 
-	// DB.REMOVE
+	// // DB.REMOVE
 
-	test("remove function works", function(t) {
-		var callback =  function (){
-			t.ok(true, "remove request to orders table worked")
-		};
-		try {
-			db.remove(table, '$1234', callback, client);
-		} catch(e) {
-			t.notOk(false, "remove request to orders table did not work");
-		}
+	// test("remove function works", function(t) {
+	// 	var callback =  function (){
+	// 		t.ok(true, "remove request to orders table worked")
+	// 	};
+	// 	try {
+	// 		db.remove(table, '$1234', callback, client);
+	// 	} catch(e) {
+	// 		t.notOk(false, "remove request to orders table did not work");
+	// 	}
 
-	    t.end();
-	});
+	//     t.end();
+	// });
 
 	// DB.EDIT
 
-	test("edit function works", function(t) {
-		var callback =  function (){
-			t.ok(true, "edit request to orders table worked")
-		};
-		try {
-			db.edit(table, testDb.mockObject2, callback, client);
-		} catch(e) {
-			t.notOk(false, "edit request to orders table did not work");
-		}
+	// test("edit function works", function(t) {
+	// 	var callback =  function (){
+	// 		t.ok(true, "edit request to orders table worked")
+	// 	};
+	// 	try {
+	// 		db.edit(table, testDb.mockObject2, callback, client);
+	// 	} catch(e) {
+	// 		t.notOk(false, "edit request to orders table did not work");
+	// 	}
 
-	    t.end();
-	});
+	//     t.end();
+	// });
 
 	test("clear table", function(t) {
-		testDb.clearTable(table);
+		testDb.clearTable();
 		t.ok(true, "table cleared");
 	    t.end();
 	});
 }
 
-testDb.createOrder('testOrders',tests)
+testDb.createOrder(tests)
