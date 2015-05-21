0612d9c71bea3c5f81ba3fae39d6a0178263d933
NataliaLKB <natalialeebaltazar@gmail.com>
2015-05-21 22:39:28 +0100

    warning message only displays on view-order on editing

--- components/orders/view_units.jsx
+++ components/orders/view_units.jsx
@@ -27,23 +27,23 @@ var units = React.createClass({
 
 				<div className="row">
 					<div className="column-3">
-						<input className="view_input" type="text" name="unit_number" defaultValue={unit.unit_number} disabled={this.props.editing ? true : false} required/>
+						<input className="view_input" type="text" name="unit_number" defaultValue={unit.unit_number} disabled={this.props.viewing ? true : false} required/>
 						{( unit.unit_id
-							? <input className="display-none" type="text" name="unit_id" defaultValue={unit.unit_id} disabled={this.props.editing ? true : false} required/>
+							? <input className="display-none" type="text" name="unit_id" defaultValue={unit.unit_id} disabled={this.props.viewing ? true : false} required/>
 							: <p className="display-none"></p>
 						)}
 					</div>
 					<div className="column-3 ">
-						<input className="view_input" type="text" name="unit_type" defaultValue={unit.unit_type} disabled={this.props.editing ? true : false} required/>
+						<input className="view_input" type="text" name="unit_type" defaultValue={unit.unit_type} disabled={this.props.viewing ? true : false} required/>
 					</div>
 					<div className="column-4">
-						<input className="view_input" type="text" name="unit_loading_reference" defaultValue={unit.unit_loading_reference} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="text" name="unit_loading_reference" defaultValue={unit.unit_loading_reference} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-3">
-						<input className="view_input" type="date" name="unit_loading_date" defaultValue={unit.unit_loading_date} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="date" name="unit_loading_date" defaultValue={unit.unit_loading_date} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-3">
-						<input className="view_input" type="time" name="unit_loading_time" defaultValue={unit.unit_loading_time} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="time" name="unit_loading_time" defaultValue={unit.unit_loading_time} disabled={this.props.viewing ? true : false}/>
 					</div>
 				</div>
 
@@ -70,22 +70,22 @@ var units = React.createClass({
 
 				<div className="row">
 					<div className="column-2">
-						<input className="view_input" type="number" name="unit_net_weight" defaultValue={unit.unit_nett_weight} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="number" name="unit_net_weight" defaultValue={unit.unit_nett_weight} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-2">
-						<input className="view_input" type="number" name="unit_gross_weight" defaultValue={unit.unit_gross_weight} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="number" name="unit_gross_weight" defaultValue={unit.unit_gross_weight} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-2">
-						<input className="view_input" type="number" name="unit_volume" defaultValue={unit.unit_volume} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="number" name="unit_volume" defaultValue={unit.unit_volume} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-6">
-						<input className="view_input" type="text" name="unit_commodity_description" defaultValue={unit.unit_commodity_description} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="text" name="unit_commodity_description" defaultValue={unit.unit_commodity_description} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-2">
-						<input className="view_input" type="number" name="unit_no_of_packages" defaultValue={unit.unit_no_of_packages} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="number" name="unit_no_of_packages" defaultValue={unit.unit_no_of_packages} disabled={this.props.viewing ? true : false}/>
 					</div>
 					<div className="column-2">
-						<input className="view_input" type="text" name="unit_kind_of_packages" defaultValue={unit.unit_kind_of_packages} disabled={this.props.editing ? true : false}/>
+						<input className="view_input" type="text" name="unit_kind_of_packages" defaultValue={unit.unit_kind_of_packages} disabled={this.props.viewing ? true : false}/>
 					</div>
 				</div>
 			</units>
