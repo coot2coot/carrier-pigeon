a95240a294786be21370b6d6a36db38e127a6fba
NataliaLKB <natalialeebaltazar@gmail.com>
2015-05-21 16:38:41 +0100

    adding validate-order tests

--- server/handlers/edit-db.js
+++ server/handlers/edit-db.js
@@ -1,5 +1,5 @@
 var parseData 	 	= require('../lib/get-form-data.js');
-var validateOrder = require('../lib/validate-order.js');
+var validateOrder = require('../lib/validate-order.js').validate;
 var validateUser = require('../lib/validate-user.js');
 var splitObject = require('../lib/split-orders-object.js');
 var db 				= require("../db-config.js");
--- tests/unit/lib/stringify-units-sql.test.js
+++ tests/unit/lib/stringify-units-sql.test.js
@@ -2,9 +2,6 @@ var test = require('tape');
 var stringify = require('../../../server/lib/stringify-units-sql.js').stringify;
 var addUnitMock = require('../mocks/add-order.js').oneUnit;
 var mockData = require('../mocks/add-order.js').stringifiedOneUnit;
-var addMultiUnitMock = require('../mocks/add-order.js').multiUnits;
-var multiUnitMockData = require('../mocks/add-order.js').stringifiedMultiUnits;
-
 
 test('Testing that stringify units in lib is', function (t) {
 	t.equals( typeof stringify, 'function', "a function");
@@ -50,6 +47,9 @@ test('when stringify data is given a result, returns data with values format', f
 * 	For adding a Multiple Units
 **********************************/
 
+var addMultiUnitMock = require('../mocks/add-order.js').multiUnits;
+var multiUnitMockData = require('../mocks/add-order.js').stringifiedMultiUnits;
+
 test('when stringify data is given a result, returns data with columns format', function (t) {
 
 	var data = stringify(addMultiUnitMock());
@@ -67,5 +67,84 @@ test('when stringify data is given a result, returns data with values format', f
 
 	t.equals( typeof data.values, 'string', "is a string");
 	t.equals( data.values, mockValues, "returned with the right string");
+	t.end();
+});
+
+
+/*********************************
+* 	Get Values tests
+**********************************/
+
+var getValues = require('../../../server/lib/stringify-units-sql.js').values;
+
+test('Testing that getValues is ', function (t) {
+	t.equals( typeof getValues, 'function', "a function");
+	t.end();
+});
+
+test('getValues returns a str', function (t) {
+	var result = getValues(addMultiUnitMock());
+
+	t.equals( typeof result, 'string', "a function");
+	t.end();
+});
+
+test('getValues returns the correct string', function (t) {
+	var result = getValues(addMultiUnitMock());
+	var testResult = "(" + multiUnitMockData().values + ")"
+
+	t.equals(result, testResult, "a function");
+	t.end();
+});
+
+/*********************************
+* 	Get Columns tests
+**********************************/
+
+var getColumns = require('../../../server/lib/stringify-units-sql.js').columns;
+
+test('Testing that getColumns is ', function (t) {
+	t.equals( typeof getColumns, 'function', "a function");
+	t.end();
+});
+
+test('getColumns returns a str', function (t) {
+	var result = getColumns(addMultiUnitMock());
+
+	t.equals( typeof result, 'string', "a function");
+	t.end();
+});
+
+test('getColumns returns the correct string', function (t) {
+	var result = getColumns(addMultiUnitMock());
+
+	t.equals(result, multiUnitMockData().columns, "a function");
+	t.end();
+});
+
+/*********************************
+* 	Has Job Number tests
+**********************************/
+
+var hasJobNumber = require('../../../server/lib/stringify-units-sql.js').hasJobNumber;
+
+test('Testing that hasJobNumber function is', function (t) {
+	t.equals( typeof hasJobNumber, 'function', "a function");
+	t.end();
+});
+
+test('hasJobNumber if given undefined', function (t) {
+	var job_number = hasJobNumber(undefined);
+	var str = "(SELECT job_number FROM orders ORDER BY job_number DESC LIMIT 1)";
+
+	t.equals( job_number, str, "returns correct string");
+	t.end();
+});
+
+test('hasJobNumber if given a job_number', function (t) {
+	var num = 55;
+	var job_number = hasJobNumber(num);
+
+	t.equals( job_number, num, "returns correct string");
 	t.end();
 });
