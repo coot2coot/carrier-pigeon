1323cff169a7c6fa18d6fd46f0c5b97757c2a15a
NataliaLKB <natalialeebaltazar@gmail.com>
2015-05-21 13:21:51 +0100

    refactoring for easier tests

--- server/lib/stringify-units-sql.js
+++ server/lib/stringify-units-sql.js
@@ -1,62 +1,84 @@
 "use strict";
 var stringify = require('./stringify-data-sql.js');
 
+function isJobNumber (number) {
+	if (number) {
+		return number;
+	} else {
+		return "(SELECT job_number FROM orders ORDER BY job_number DESC LIMIT 1)";
+	}
+}
+
+function stringifyOneUnit (object) {
+	var data;
+	var job_number = isJobNumber(object.job_number);
+
+	data = stringify(object);
+	var str = data.values.slice(0, -2) + job_number;
+
+	data.values = str;
+	return data;
+}
+
+function getColumns (object) {
+	var props,
+		arr = [],
+		str = "";
+
+	for (props in object) {
+		arr.push(props);
+	}
+
+	str = arr.join();
+
+	return str;
+}
+
+function getValues (object) {
+	var values = [],
+	    length = object.unit_number.length,
+	    i;
+
+	for (i = 0; i < length; i ++) {
+		var props,
+	      	arr = [],
+	      	str = "";
+			  
+	  	for(props in object) {
+			if (typeof object[props] === "object"){
+		      	var propValue = "'" + object[props][i] + "'";
+		      	
+		      	if(propValue === "''") {
+		        	propValue = "null";
+		      	}
+			  	arr.push(propValue);
+			}
+		}
+
+		var job_number = isJobNumber(object.job_number);
+
+		str += "(" + arr.join() + "," + job_number + ")";
+	  	values.push(str);
+	  	arr = [];
+	}
+	return values.join();
+}
+
 module.exports = function (units){
 	var data = {};
 
-	if(typeof units["unit_type"] === "object"){
+	if(typeof units.unit_type === "object"){
 		data.columns = "";
 		data.values = "";
 
-		var columns = [],
-		    values = [],
-		    length = units.unit_number.length,
-		    i;
+		var valueStr = getValues(units);
 
-		for (i = 0; i < length; i ++) {
-		  	var props,
-		      	arr = [],
-		      	str = "";
-				  
-		  	for(props in units) {
-				if (i === 0){
-				  	columns.push(props);
-				}
-				if (typeof units[props] === "object"){
-			      	var propValue = "'" + units[props][i] + "'";
-			      	
-			      	if(propValue === "''") {
-			        	propValue = "null";
-			      	}
-				  	arr.push(propValue);
-				}
-		  	}
-		  	var job_number = "(SELECT job_number FROM orders ORDER BY job_number DESC LIMIT 1)";
-		  	if (units.job_number) {
-		  		job_number = units.job_number;
-		  	}
-		  	str += "(" + arr.join() + "," + job_number + ")";
-		  	values.push(str);
-		  	arr = [];
-		}
-
-		var valueStr = values.join()
-
-		data.columns = columns.join();
+		data.columns = getColumns(units);
 		data.values = valueStr.substring(1, valueStr.length-1);
 		return data;
 		
 	} else {
-	  	var job_number = "(SELECT job_number FROM orders ORDER BY job_number DESC LIMIT 1)";
-	  	
-	  	if (units.job_number) {
-	  		job_number = units.job_number;
-	  	}
-
-		data = stringify(units);
-		var str = data.values.slice(0, -2) + job_number;
-
-		data.values = str;
-		return data;
+	  	return stringifyOneUnit(units);
 	}
 }
+
