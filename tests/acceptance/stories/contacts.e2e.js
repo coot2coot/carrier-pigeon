abc67aa2f668a2d2b269a2fd5716187b94bb6f0a
benjaminlees <benji.man.lees@gmail.com>
2015-05-26 16:58:20 +0100

    acceptance tests for contacts

--- tests/acceptance/stories/contacts.e2e.js
+++ tests/acceptance/stories/contacts.e2e.js
@@ -52,6 +52,69 @@ function landingTests (wd, capability, remote) {
 
                 .nodeify(done);
         });
+
+        it("Should be able to add a contact", function(done) {
+            browser
+                .get("http://localhost:8000/#/contacts")
+                .waitForElementByCssSelector(".add.blue")
+                .click()
+                .waitForElementByCssSelector("input[name='company_name']")
+                .sendKeys("fake", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='address_line']")
+                .sendKeys("17 abercorn", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='city']")
+                .sendKeys("london", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='country']")
+                .sendKeys("uk", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='postcode']")
+                .sendKeys("uk", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='telephone']")
+                .sendKeys("98376491873", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='name']")
+                .sendKeys("david", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByCssSelector("input[name='email']")
+                .sendKeys("david@gaf.com", function (err) {
+                    if (err) console.log(err);
+                })
+                .elementByTagName("form")
+                .submit(function (err) {
+                    if (err) console.log(err);
+                })
+                .url(function(err, url) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    expect(url).to.have.string("true");
+                })
+                .nodeify(done);
+        });
+    it("Should be able to delete a contact", function(done) {
+            browser
+                .get("http://localhost:8000/#/contacts")
+                .waitForElementByCssSelector("a[data-reactid='.0.1.2.0.2.1.$1first.0']")
+                .click()
+                .waitForElementByCssSelector("a[data-reactid='.0.2.1.0.0']")
+                .click()
+                .waitForElementByCssSelector("a[data-reactid='.0.2.0.0.0.1.2']")
+                .click()
+                .eval("window.location.href")
+                    .should.eventually.include('true')
+                .nodeify(done);
+        });
     });
 };
 
