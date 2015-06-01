44c5145d737587992e10c9df7547056917d1c636
NataliaLKB <natalialeebaltazar@gmail.com>
2015-06-01 18:10:14 +0100

    making it so the tab that is active, is lighter than the other tabs in the navbar

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,6 @@ var React       = require('react');
 var Router      = require('react-router');
 
 var DefaultRoute= Router.DefaultRoute;
-var Link        = Router.Link;
 var Route       = Router.Route;
 var RouteHandler= Router.RouteHandler;
 
