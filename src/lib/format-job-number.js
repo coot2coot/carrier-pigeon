f817207019974639e709cdd2570fa8ff72e966dc
NataliaLKB <natalialeebaltazar@gmail.com>
2015-07-27 09:49:09 +0100

    adding if statement for dbId to make sure not to add things with real data

--- src/lib/format-job-number.js
+++ src/lib/format-job-number.js
@@ -6,21 +6,24 @@
 (function () {
 	"use strict"
 
-	var formatJobNumber = function (dbId) {
+	function formatJobNumber (dbId) {
 
-		var idLength 	= dbId.toString().length;
-		var today 		= new Date();
-		var mm 			= ("0" + (today.getMonth()+1)).slice(-2);
-		var yy 			= today.getFullYear().toString().slice(-2);
+		if (dbId) {
 
-		if (idLength < 5) {
-	  
-			var id = ("0000" + dbId).slice(-4);
-		} else {
-			var id = ("00000" + dbId).slice(-5);
+			var idLength 	= dbId.toString().length;
+			var today 		= new Date();
+			var mm 			= ("0" + (today.getMonth()+1)).slice(-2);
+			var yy 			= today.getFullYear().toString().slice(-2);
+
+			if (idLength < 5) {
+		  
+				var id = ("0000" + dbId).slice(-4);
+			} else {
+				var id = ("00000" + dbId).slice(-5);
+			}
+
+			return yy + mm + id;
 		}
-
-		return yy + mm + id;
 	}
 
 	module.exports = formatJobNumber;
