dd904c6a74c5ec071c8ba187b05def0d30988395
benjamin lees <benji.man.lees@gmail.com>
2015-08-27 22:03:27 +0100

    fixing tests

--- tests/integration/mocks/contacts.js
+++ tests/integration/mocks/contacts.js
@@ -6,12 +6,16 @@ mock = {
         singleValueObject: {
             name: "dave",
             vat_number: "456ghtd",
-            city: "london",
-            telephone: "0203467893"
+            city: "london"
         },
         multipleValuesObject: {
             date: "2015-08-26",
             message: "what"
+        },
+        multipleValuesObject2: {
+            name: "dav",
+            telephone: "1234",
+            email: "dav@dd.com"
         }
     },
     edit : {
@@ -19,14 +23,18 @@ mock = {
         singleValueObject: {
             contact_id: "false",
             vat_number: "456ghtd",
-            city: "london",
-            telephone: "0203467893"
+            city: "london"
         },
         multipleValuesObject: {
             date: "2015-08-26",
             message: "what",
             reminder_id: "",
             contact_reminders_id: "false"
+        },
+        multipleValuesObject2: {
+            name: "dav",
+            telephone: "1234",
+            email: "dav@dd.com"
         },
         items_to_remove: ''
     }
