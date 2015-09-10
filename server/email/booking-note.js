610cb21817ecde1dc9637374a1d55e8077de14bd
benjamin lees <benji.man.lees@gmail.com>
2015-09-10 09:38:39 +0100

    small fix

--- server/email/booking-note.js
+++ server/email/booking-note.js
@@ -2,8 +2,8 @@ var url 		= "http://carrierpigeonfac-se-env.elasticbeanstalk.com/";
 var formatJobId = require('../../src/lib/format-job-number.js');
 
 function bookingRequestEmail (order, sender) {
-	var jobNumber 	= formatJobId(order.job_number);
-	
+	var jobNumber 	= formatJobId(order.job_number, order.date);
+
 	var string = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
 	'<html xmlns="http://www.w3.org/1999/xhtml">' +
 	 	'<head>' +
